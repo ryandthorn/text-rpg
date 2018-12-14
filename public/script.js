bc5b54077052ed9c1bbb72da8492a2b5be4427a5
Ryan Thorn <ryandthorn@gmail.com>
2018-12-14 14:06:17 -0800

    Initial setup with express

---
+++ public/script.js
@@ -0,0 +1,3 @@
+'use strict';
+
+console.log('You are in /public/script.js')
---
+++ server.js
@@ -0,0 +1,5 @@
+'use strict';
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+app.listen(process.env.PORT || 8080);
