09cc32ef4c446bdd11d3b897c05a51aecbd60ead
Ryan Thorn <ryandthorn@gmail.com>
2018-12-23 11:13:15 -0800

    Write setScrollPosition() for combat log

---
+++ test-fight-script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function setScrollPosition() {
+  $('.combat--log').scrollTop(999999999);
+}
+
+$('.btn--character').click(event => {
+  event.preventDefault();
+})
+
+$('.btn--combat').click(event => {
+  event.preventDefault();
+})
