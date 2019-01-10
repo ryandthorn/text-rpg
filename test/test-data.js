806ea292858bb30e090782bb9da69e4e64779684
Ryan Thorn <ryandthorn@gmail.com>
2019-01-10 09:06:11 -0800

    Add characterData and enemyData to test-data

--- test/test-data.js
+++ test/test-data.js
@@ -92,95 +92,201 @@ const storyData = {
 };
 
 const characterData = {
-  create: function(selection) {
-    const newCharacter = {};
-    if (selection === 'mage') {
-      newCharacter.class = 'Mage';
-      newCharacter.attributes = {
-        hp: 10,
-        mp: 18,
-        maxHP: 10,
-        maxMP: 18,
-        str: 6,
-        agi: 10,
-        mag: 12,
-        con: 8
-      };
-      newCharacter.skills = {
-        phyDamage: -1,
-        phyResist: -1,
-        accuracy: 1,
-        evasion: 1,
-        magDamage: 2,
-        magResist: 2,
-        fortitude: 0,
-        damReduce: 0
-      };
-    } else if (selection === 'thief') {
-      newCharacter.class = 'Thief',
-      newCharacter.attributes = {
-        hp: 14,
-        mp: 10,
-        maxHP: 14,
-        maxMP: 10,
-        str: 8,
-        agi: 12,
-        mag: 8,
-        con: 10
-      };
-      newCharacter.skills = {
-        phyDamage: 0,
-        phyResist: 0,
-        accuracy: 2,
-        evasion: 2,
-        magDamage: 0,
-        magResist: 0,
-        fortitude: 1,
-        damReduce: 1
-      };
-    } else if (selection === 'warrior') {
-      newCharacter.class = 'Warrior';
-      newCharacter.attributes = {
-        hp: 18,
-        mp: 6,
-        maxHP: 18,
-        maxMP: 6,
-        str: 10,
-        agi: 8,
-        mag: 6,
-        con: 12
+  mage: {
+    bookmark: {
+      chapter: 'chapter1',
+      scene: 'scene1',
+      next: [
+        {
+          chapter: 'chapter1',
+          scene: 'scene2'
+        }
+      ]
+    },
+    class: 'Mage',
+    attributes: {
+      hp: 8,
+      mp: 12,
+      maxHP: 8,
+      maxMP: 12,
+      str: 6,
+      agi: 10,
+      mag: 12,
+      con: 8
+    },
+    skills: {
+      phyDamage: -1,
+      phyResist: -1,
+      accuracy: 1,
+      evasion: 1,
+      magDamage: 2,
+      magResist: 2,
+      fortitude: 0,
+      damReduce: 0
+    },
+    actions: {
+      attack: {
+        info: `Physical attack for 1-6 damage`,
+        damage: [1, 6]
       },
-      newCharacter.skills = {
-        phyDamage: 1,
-        phyResist: 1,
-        accuracy: 0,
-        evasion: 0,
-        magDamage: -1,
-        magResist: -1,
-        fortitude: 2,
-        damReduce: 2
-      };
+      defend: {
+        info: `Take a defensive stance. +5 defense bonus vs enemy to-hit roll.`,
+        damage: [1, 4],
+        defendBonus: 0
+      },
+      missile: {
+        info: `Shoot a magic missile at your opponent. 1-6 + 4 damage (-2 MP)`,
+        damage: [1, 8, 4],
+        mpCost: 2
+      }
     }
-    return newCharacter;      
   },
-  read: function(){
-    return this.character;
-  },
-  update: function(updateObj) {
-    this.character.attributes.hp += Number(updateObj.hp);
-    this.character.attributes.mp += Number(updateObj.mp);
-    if (this.character.attributes.hp > this.character.attributes.maxHP) {
-      this.character.attributes.hp = this.character.attributes.maxHP;
+  thief: {
+    bookmark: {
+      chapter: 'chapter1',
+      scene: 'scene1',
+      next: [
+        {
+          chapter: 'chapter1',
+          scene: 'scene2'
+        }
+      ]
+    },
+    class: 'Thief',
+    attributes: {
+      hp: 10,
+      mp: 10,
+      maxHP: 10,
+      maxMP: 10,
+      str: 8,
+      agi: 12,
+      mag: 8,
+      con: 10
+    },
+    skills: {
+      phyDamage: 0,
+      phyResist: 0,
+      accuracy: 2,
+      evasion: 2,
+      magDamage: 0,
+      magResist: 0,
+      fortitude: 1,
+      damReduce: 1
+    },
+    actions: {
+      attack: {
+        info: `Physical attack for 1-6 damage`,
+        damage: [1, 6]
+      },
+      defend: {
+        info: `Take a defensive stance. +5 defense bonus vs enemy to-hit roll.`,
+        damage: [1, 4],
+        defendBonus: 0
+      },
+      dblStrike: {
+        info: `Attack twice this turn (-2 MP)`,
+        damage: [1, 6],
+        mpCost: 2
+      }
     }
-    if (this.character.attributes.mp > this.character.attributes.maxMP) {
-      this.character.attributes.mp = this.character.attributes.maxMP;
-    }
-    // if (hp < =0) {lose game}
-    return this.character;
   },
-  delete: function() {
-    delete this.character;
+  warrior: {
+    bookmark: {
+      chapter: 'chapter1',
+      scene: 'scene1',
+      next: [
+        {
+          chapter: 'chapter1',
+          scene: 'scene2'
+        }
+      ]
+    },
+    class: 'Warrior',
+    attributes: {
+      hp: 12,
+      mp: 8,
+      maxHP: 12,
+      maxMP: 8,
+      str: 10,
+      agi: 8,
+      mag: 6,
+      con: 12
+    },
+    skills: {
+      phyDamage: 1,
+      phyResist: 1,
+      accuracy: 0,
+      evasion: 0,
+      magDamage: -1,
+      magResist: -1,
+      fortitude: 2,
+      damReduce: 2
+    },
+    actions: {
+      attack: {
+        info: `Physical attack for 1-6 damage`,
+        damage: [1, 6]
+      },
+      defend: {
+        info: `Take a defensive stance. +5 defense bonus vs enemy to-hit roll.`,
+        damage: [1, 4],
+        defendBonus: 0
+      },
+      smash: {
+        info: `An unavoidable crushing blow. 3-5 + 4 damage (-2 MP)`,
+        damage: [3, 5, 4],
+        mpCost: 2
+      }
+    }
   }
 };
 
-module.exports = { characterData, storyData }; 
+const enemyData = {
+  name: "Troglodyte",
+  imgSrc: "images/grzyb-troglodyte-full.jpg",
+  imgSrcSet: "images/grzyb-troglodyte-tiny.jpg 300w, images/grzyb-troglodyte-small.jpg 600w, images/grzyb-troglodyte-full.jpg 900w",
+  alt: "This eyeless, misshapen creature wields a sharp spear.",
+  attributes: {
+    hp: 8,
+    mp: 0,
+    maxHP: 8,
+    maxMP: 0,
+    str: 6,
+    agi: 8,
+    mag: 6,
+    con: 6
+  },
+  skills: {
+    phyDamage: -1,
+    phyResist: -1,
+    accuracy: 0,
+    evasion: 0,
+    magDamage: -1,
+    magResist: -1,
+    fortitude: -1,
+    damReduce: -1
+  },
+  actions: {
+    attack: {
+      text: [
+        "The troglodyte lowers its spear...",
+        "The troglodyte shifts its weight forward...",
+        "The troglodyte hisses..."
+      ],
+      damage: [
+        1,
+        4
+      ]
+    },
+    defend: {
+      text: [
+        "The troglodyte takes a defensive stance...",
+        "The troglodyte steps back...",
+        "The troglodyte hesitates..."
+      ],
+      defendBonus: 0
+    }
+  }
+}
+
+module.exports = { characterData, storyData, enemyData }; 
