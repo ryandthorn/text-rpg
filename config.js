539c9523b4e0912f05e316c66d75ac9314c6af9d
Ryan Thorn <ryandthorn@gmail.com>
2019-01-11 10:21:16 -0800

    Update test database URL in config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
 exports.PORT = process.env.PORT || 8080;
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://test:sconnie1@ds213183.mlab.com:13183/text-rpg';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/text-rpg-test';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://test:sconnie1@ds253804.mlab.com:53804/text-rpg-test';
